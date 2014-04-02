91487e5fbd8dedc5d6681b639ee677671f756665
Anilkumar Ranganagoudra <anilkuma@mumble-14.cs.wisc.edu>
2014-04-01 22:30:33 -0500

    Added p3 linux files

--- p3/linux/mem.c
+++ p3/linux/mem.c
@@ -4,7 +4,9 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include "mem.h"
+#include <math.h>
 
+#include "helper.h"
 #define DEBUG
 
 #define FILEPATH "/dev/zero"
@@ -13,7 +15,7 @@ volatile int mem_call = 0;
 int m_error;
 
 //Mem_init
-int Mem_Init(int sizeOfRegion)
+extern int Mem_Init(int sizeOfRegion)
 {
 	mem_call++;
 	if(mem_call > 1 || sizeOfRegion <= 0)
@@ -23,47 +25,61 @@ int Mem_Init(int sizeOfRegion)
 	}
 
 	//sizeOfRegion (in bytes) needs to be evenly divisible by the page size
-	int round_sz = 0;
-	int pg_sz = getpagesize();
+	int round_sz = (int) ceil (sizeOfRegion/ getpagesize());
+	/*int pg_sz = getpagesize();
 	int pg_div = (int) sizeOfRegion / pg_sz;
 
 	if(!pg_div)
 		round_sz = pg_sz;
 	else 
 		round_sz = (int) (sizeOfRegion % getpagesize());
-
+*/
 	#ifdef DEBUG
-		printf("page size: %d\n", pg_sz);
+		printf("page size: %d\n", round_sz);
 	#endif
 	
 	//Open a file for mapping
 	int fd = open(FILEPATH, O_RDWR);
 	
 	//Call the mmap func: mmap(addr, len, prot, flags, fildes, off);
-	void* mem_ptr = mmap(NULL, sizeOfRegion, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+	void* mem_ptr = mmap(NULL, round_sz, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
 	
 	if (mem_ptr == MAP_FAILED)
 	{
-		m_error = E_BAD_POINTER; 
+		m_error = E_BAD_ARGS; 
 		return (-1);
 	}
 	
 	//close the device (don't worry, mapping should be unaffected)
 	close(fd);
+	head = mem_ptr;
+	head->size = round_sz;
+	head->next = NULL;
 	return 0;
 
 }
 
-void* Mem_Alloc(int size, int style)
-{
+extern void* Mem_Alloc(int size){
+	
+	void* ptr = list_add (size);
+	if(ptr == NULL){
+		m_error = E_NO_SPACE;
+		return(NULL);
+	}
+	else 
+		return ptr;
+
 }
 
 
-int Mem_Free(void* ptr)
+extern int Mem_Free(void* ptr)
 {
+	int ret_free = list_free(ptr);
+	return ret_free;
 }
 
 
 void Mem_Dump()
 {
+
 }
--- p3/linux/p3.c
+++ p3/linux/p3.c
@@ -1,10 +1,12 @@
-#include <stdio.h> 
-#include <stdlib.h>
-#include <unistd.h>
+#include "mem.h"
+#include "helper.h"
 
 int main(int argc, char** argv)
 {
-	printf("page size: %d\n", getpagesize());
-
+	Mem_Init(4000);
+	int* a= (int *)Mem_alloc(sizeof(int));
+	a=10;
+	printf("a=%d, %p", *a, a);
+	Mem_Free(a);
 	return 0;
 }
